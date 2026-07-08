b68ce65bf9c5c0ce49cb62ae7ff1ceb1dc612885
Jokhid <hidalgo.j.carlos@gmail.com>
2026-07-08 17:03:15 +0200

    Defer smooth scroll initialization

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -3,22 +3,46 @@ import Lenis from "lenis";
 
 export function SmoothScroll() {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.25,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-    });
+    type IdleWindow = Window & {
+      requestIdleCallback?: (callback: () => void, options?: { timeout: number }) => number;
+      cancelIdleCallback?: (id: number) => void;
+    };
 
-    let rafId: number;
-    const raf = (time: number) => {
-      lenis.raf(time);
+    const idleWindow = window as IdleWindow;
+    let lenis: Lenis | null = null;
+    let rafId = 0;
+    let idleId = 0;
+    let timeoutId = 0;
+    let started = false;
+
+    const start = () => {
+      if (started) return;
+      started = true;
+
+      lenis = new Lenis({
+        duration: 1.25,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smoothWheel: true,
+      });
+
+      const raf = (time: number) => {
+        lenis?.raf(time);
+        rafId = requestAnimationFrame(raf);
+      };
       rafId = requestAnimationFrame(raf);
     };
-    rafId = requestAnimationFrame(raf);
+
+    if (idleWindow.requestIdleCallback) {
+      idleId = idleWindow.requestIdleCallback(start, { timeout: 2000 });
+    } else {
+      timeoutId = window.setTimeout(start, 800);
+    }
 
     return () => {
-      cancelAnimationFrame(rafId);
-      lenis.destroy();
+      if (idleId && idleWindow.cancelIdleCallback) idleWindow.cancelIdleCallback(idleId);
+      if (timeoutId) window.clearTimeout(timeoutId);
+      if (rafId) cancelAnimationFrame(rafId);
+      lenis?.destroy();
     };
   }, []);
 
